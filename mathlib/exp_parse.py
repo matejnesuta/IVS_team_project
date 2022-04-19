6778c70d86eed874c3039f2a64f4bab714c4e49e
martindzida <martin.d14@email.cz>
2022-04-19 17:27:27 +0200

    Additional bracket function, parsing operands of nthrt and log
    Added function for locating complementary bracket and implemented find_oprnds for nthrt and log operators.

--- mathlib/exp_parse.py
+++ mathlib/exp_parse.py
@@ -32,24 +32,55 @@ def str_to_float(s):
     return res
 
 
-#TODO: probably fine for now, but it doesnt check the content of the brackets
 """
 CHECKING PAIRS OF BRACKETS
 brief: Checking if there is always a complementary bracket
 param: str exp
-return True / False
+return: True / False
 """
 def bracket_pair_check(exp):
     n_left_br = 0
     n_right_br = 0
     for c in exp:
-        if c == "(":
+        if c == '(':
             n_left_br += 1
-        if c == ")":
+        if c == ')':
             n_right_br += 1
         if n_right_br > n_left_br:
             return False
     return True if n_left_br == n_right_br else False
+
+
+"""
+FIND COMPLEMENTARY BRACKET
+brief: Based on the index of the bracket finds its complementary bracket
+param: str exp, int first_i, bool rev
+return: int sec_i
+"""
+def find_comp_br(exp, br_i, rev):
+    n_left_br = 0
+    n_right_br = 0
+
+    #iterating through expression backwards
+    if rev:
+        n_right_br = 1
+        for i in range(br_i - 1, -1, -1):
+            if exp[i] == '(':
+                n_left_br += 1
+            if exp[i] == ')':
+                n_right_br += 1
+            if n_left_br == n_right_br:
+                return i 
+
+    else:
+        n_left_br = 1
+        for i in range(br_i + 1, len(exp)):
+            if exp[i] == '(':
+                n_left_br += 1
+            if exp[i] == ')':
+                n_right_br += 1
+            if n_left_br == n_right_br:
+                return i
 
 
 """
@@ -68,32 +99,47 @@ def find_oprtrs(exp):
             found_oprtrs[op_i] = op
             f_p = op_i + 1
         f_p = 0
-    return found_oprtrs
+    #sorting dict of operators by keys(index)
+    oprtrs_sorted = {k:v for k,v in sorted(found_oprtrs.items())}
+    return oprtrs_sorted
 
 
 """
 FIND OPERANDS OF THE EXPRESSION
 brief: Looking for opearnds of the operation
 param: str exp, int op_i, str op
-return: list[l_oprnd, r_oprnd] 
+return: dict{l_op: l_oprnd, r_op: r_oprnd} 
 """
 def find_oprnds(exp, op_i, op):
-    oprnds = []
-    #some kind of a switch
+    oprnds = {}
     match op:
         case '!':
-            print('!')
+            pass
         case 'nthrt':
-            print('rt')
+            #arguments of nth root must be in brackets 
+            #the offset stands for len(nthrt)
+            if exp[op_i + 5] != '(':
+                return False
+            else:
+                end_br = find_comp_br(exp, op_i + 5, False)
+                params = exp[op_i + 6:end_br] 
+                ops = params.split(',')
+                oprnds['l_oprnd'] = ops[0]
+                oprnds['r_oprnd'] = ops[1]
         case 'log':
-            print('log')
+            #arguments of logarithm must be in brackets 
+            #the offset is stands for len(log)
+            if exp[op_i + 3] != '(':
+                return False
+            else:
+                end_br = find_comp_br(exp, op_i + 3, False)
+                params = exp[op_i + 4:end_br] 
+                ops = params.split(',')
+                oprnds['l_oprnd'] = ops[0]
+                oprnds['r_oprnd'] = ops[1]
         #default case
-        #case _:
-
-    l_part = exp[0:op_i]
-    r_part = exp[op_i + 1:]
-    oprnds.append(l_part)
-    oprnds.append(r_part)
+        case _:
+            pass
     return oprnds
 
 
@@ -112,6 +158,7 @@ EXPRESSION PARSER
 brief: Parsing the whole expression and dividing it to the seperate operations
 param: str exp, dict {oprtrs}
 return: dict{} of elementary operations and their order of execution
+list[{i: oprtr, pr: n}, {l_op: l_oprnd, r_op: r_oprnd}]
 """
 def exp_parse(exp):
     #validation part
@@ -119,8 +166,7 @@ def exp_parse(exp):
         print("Missing bracket")
         return False
     
-
-    exp_oprts = find_oprtrs(exp, oprtrs_set)
+    exp_oprts = find_oprtrs(exp)
     for op_i in exp_oprts:
         print(find_oprnds(exp, op_i, exp_oprts[op_i]))
     return
