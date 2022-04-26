364ac43facc6b59c18b12c57eb67a48211f61922
matejnesuta <matejnetusa@gmail.com>
2022-04-26 16:01:31 +0200

    profiling skoro hotov

--- calculator/profiling.py
+++ calculator/profiling.py
@@ -1,6 +1,7 @@
 import re
 import sys
 import cProfile
+from mathlib.calc_exp import calc_output, is_neg_oprnd, to_neg_oprnd
 
 
 lines = sys.stdin.readlines()
@@ -11,10 +12,43 @@ for i in lines:
 
 nums = re.sub(r"[ \t\n]+", "+", nums)
 
+minus = (nums.split("+"))
+nums = ""
+for i in minus:
+    if (i[0]=='-'):
+        nums+=to_neg_oprnd(i)+"+"
+    else:
+        nums+=i+"+"    
+
+nums=nums[:-1]
+
+exps = (nums.split("+"))
+expNums = ""
+for i in exps:
+    i+="^2"
+    expNums+=i+"+"
+
+expNums=expNums[:-1]
+
+count = str(nums.count("+")+1)
+
 profile = cProfile.Profile()
 profile.enable()
 
-print(nums)
+sum = str(calc_output(nums))
+avg = str(calc_output(sum+"/"+count))
+
+powSum = str(calc_output(expNums))
+b = str(calc_output(count+"*"+avg+"^2"))
+c = str(calc_output(powSum+"-"+b))
+sum2 = str(calc_output(sum+"-1"))
+d = str(calc_output(c+"/"+sum2))
+
+#TODO: FLIP CISEL TADY
+result = str(calc_output("nrt(2,"+d+")"))
+
+print(result)
+# print(nums)
 
 profile.disable()
 profile.print_stats(sort='time')
