de336f7577337559db583f1087309cbdb50f5863
ilimarx <tomarkelov@gmail.com>
2022-04-29 04:02:44 +0200

    Asi funkcni instalator

--- src/installer.sh
+++ src/installer.sh
@@ -4,7 +4,7 @@ cp  calculator.py ../installer/usr/share/calculator/calculator.py && chmod +x ..
 cp -r  ./mathlib ../installer/usr/share/calculator/mathlib
 cp  gui.ui ../installer/usr/share/calculator/gui.ui
 mkdir -p ../installer/usr/local/bin 2>/dev/null
-ln -sf /usr/share/calculator/main.py ../installer/usr/local/bin/calculator
+ln -sf /usr/share/calculator/calculator.py ../installer/usr/local/bin/calculator
 mkdir ../installer/tmp 2>/dev/null
 cp requirements.txt ../installer/tmp/requirements.txt
 chmod +x ../installer/DEBIAN/postinst
